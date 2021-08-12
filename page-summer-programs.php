8ee9f2262432302664f1df62f0d73f42d07e827d
v.kovtun <v.kovtun@ies.org.ua>
2021-08-12 17:59:04 +0300

    added summer programs page

---
+++ page-summer-programs.php
@@ -0,0 +1,148 @@
+<?php
+/* Template Name: summer-programs */
+get_header();
+?>
+
+<style>
+    .card {
+        width: 100%;
+        max-width: 400px;
+    }
+    .col-sm-4 {
+        padding-bottom: 30px;
+    }
+    .btn {
+        margin-bottom: 10px;
+    }
+</style>
+
+<main class="main side-elements">
+    <div class="inner_wrapper">
+        <h1>Summer STEM Programs (Ages 3-12+)</h1>
+        <ul>
+            <li>Fun, hands-on, <b>in-person</b> STEM learning with LEGO, Minecraft, and more!</li>
+            <li>5-day programs (with limited "single day" slots also available)</li>
+            <li>Sibling and multi-session discounts!</li>
+            <li><b>NEW: Dedicated parent workspace with WiFi!</b> (restrictions apply; call for details)</li>
+        </ul>
+
+        <a href="https://inventorstemschool.jumbula.com/view#/home" class="btn btn-primary" style="display: block; width: 100%; max-width: 300px; margin: 15px auto;">Explore All Programs</a>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/STEM%20Dungeons%20and%20Dragons-B.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong> STEM Dungeons and Dragons (M-W-F)
+                                <br>
+                                <strong>Ages: </strong> 12+
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/STEMDungeonsandDragons14pmMF" class="btn btn-primary">Info/Register</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/Engineering%20Challenges-B.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong>  Engineering Challenges
+                                <br>
+                                <strong>Ages: </strong> 10+
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/EngineeringChallengeswithLegoMindstorms14pmMF" class="btn btn-primary">Info/Register</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/Cooperative%20Minecraft-D.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong>  Cooperative Minecraft
+                                <br>
+                                <strong>Ages: </strong>  10+ or 7+
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/CooperativeMinecraft10" class="btn btn-primary">Info/Register (10+) (T, Th)</a>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/5DayCooperativeMinecraft1014pmMF" class="btn btn-primary">Info/Register (10+) (M-F)</a>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/CooperativeMinecraft7" class="btn btn-primary">Info/Register (7+) </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/Architecture%20Lego%20Building.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong>  Architecture Lego Building
+                                <br>
+                                <strong>Ages: </strong>  8-15+
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/ArchitectureLegoBuilding" class="btn btn-primary">Info/Register</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/Minecraft%20Adventures-B.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong> Minecraft Adventures
+                                <br>
+                                <strong>Ages: </strong> 7-9 years old
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/MinecraftAdventures9am12pmMF" class="btn btn-primary">Info/Register</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/Junior%20Engineers.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong> Junior Engineers
+                                <br>
+                                <strong>Ages: </strong> 7+
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/JuniorEngineers" class="btn btn-primary">Info/Register</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/STEM%20Explorers-B.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong> STEM Explorers
+                                <br>
+                                <strong>Ages: </strong>  5-6 years old
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/STEMExplorers9am12pmMF" class="btn btn-primary">Info/Register (Mornings)</a>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/STEMExplorers14pmMF" class="btn btn-primary">Info/Register (Afternoons)</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <img src="https://www2.inventorstemschool.com/hubfs/Canva%20images/Mini%20Investigators-B.jpg" class="card-img-top" alt="Inventor">
+                        <div class="card-body">
+                            <p class="card-title">
+                                <strong>Program:</strong> Mini Investigators
+                                <br>
+                                <strong>Ages: </strong> 3-4 years old
+                            </p>
+                            <a href="https://inventorstemschool.jumbula.com/Summer2021/MiniInvestigators1011amMF" class="btn btn-primary">Info/Register</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</main>
+
+
+<?php get_footer(); ?>
