6335e3b46b90480fdedbad3fb7b029d551878d9e
v.kovtun <v.kovtun@ies.org.ua>
2021-08-12 17:51:30 +0300

    added pages: stem-senior, page online programs

---
+++ page-online-programs.php
@@ -0,0 +1,180 @@
+<?php
+/* Template Name: online-programs */
+
+get_header();
+?>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<style>
+    .online-programs-imgs  {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .online-programs-imgs a {
+        display: block;
+        width: 33%;
+    }
+    .online-programs-imgs a img {
+        display:block;
+        width: 100%;
+        height: auto;
+    }
+    .hbspt-form {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+    }
+</style>
+<style>
+    .online-programs-imgs  {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .online-programs-imgs a {
+        display: block;
+        width: 33%;
+    }
+    .online-programs-imgs a img {
+        display:block;
+        width: 100%;
+        height: auto;
+    }
+    .hbspt-form {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+    }
+</style>
+
+<main class="main side-elements">
+    <div class="inner_wrapper">
+        <h1>Programming with Minecraft: Education Edition - ONLINE</h1>
+
+        <p>
+            Our online <i>Programming with Minecraft</i> is designed specifically for remote learning. During each weekly 90-minute lesson, teachers provide synchronous instruction and personalized guidance to students <u>in real-time</u> as they apply their new knowledge to work through programming challenges in Minecraft.
+        </p>
+        <div id="accordion">
+            <h3>Program Structure</h3>
+            <div>
+                <p>
+                    30 lessons divided into 6 sequential Sections <br>
+
+                    Each lesson is 1h 45m <br>
+
+                    Enrollment is on a per-Section basis, and new cohorts open regularly. <br>
+
+                    Cost per Section: $300 <br>
+                </p>
+            </div>
+            <h3>Curriculum Overview</h3>
+            <div>
+                <p>
+                    Recommended for students age 8-11. <br>
+
+                    Students will automate functions, test and correct programming errors, and create their own games in the Minecraft virtual world. <br>
+
+                    Students operate in the MakeCode programming environment and configure code blocks that can be converted into JavaScript with one click. <br>
+
+                    Students learn fundamental programming skills and develop logical and algorithmic thinking. <br>
+                </p>
+            </div>
+            <h3>Section 1</h3>
+            <div>
+                <p>
+                    Introduction to MakeCode, events and listeners, and coordinates
+                </p>
+            </div>
+            <h3>Section 2</h3>
+            <div>
+                <p>
+                    Using variables, conditional statements, loops; introduction to advanced tools in MakeCode
+                </p>
+            </div>
+            <h3>Section 3</h3>
+            <div>
+                <p>
+                    Introduction to Boolean operators; application of coordinates, conditional statements, and variables to create a game environment
+                </p>
+            </div>
+            <h3>Section 4</h3>
+            <div>
+                <p>
+                    Fibonacci numbers, arrays, remainders, and modeling
+                </p>
+            </div>
+            <h3>Section 5</h3>
+            <div>
+                <p>
+                    Arrays, functions, automation for animation
+                </p>
+            </div>
+            <h3>Section 6</h3>
+            <div>
+                <p>
+                    Random numbers and selectors; introduction to educational game construction
+                </p>
+            </div>
+        </div>
+        <div class="online-programs-imgs">
+            <a href="#lightshot1"><img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide8.png" alt="inventor"></a>
+            <a href="#lightshot2"><img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide7.png" alt="inventor"></a>
+            <a href="#lightshot3"><img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide4.png" alt="inventor"></a>
+            <a href="#lightshot4"><img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide9.png" alt="inventor"></a>
+            <a href="#lightshot5"><img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide12.png" alt="inventor"></a>
+            <a href="#lightshot6"><img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide11-1.png" alt="inventor"></a>
+        </div>
+
+        <h2 style="margin-top: 20px;">Enrollment now open!
+            Next cohort starts March 31
+        </h2>
+
+
+    </div>
+</main>
+
+<!-- small gallery -->
+<a href="#_" id="lightshot1" class="lightshot">
+    <img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide8.png" alt="Bigger photo">
+</a>
+<a href="#_" id="lightshot2" class="lightshot">
+    <img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide7.png" alt="Bigger photo">
+</a>
+<a href="#_" id="lightshot3" class="lightshot">
+    <img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide4.png" alt="Bigger photo">
+</a>
+<a href="#_" id="lightshot4" class="lightshot">
+    <img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide9.png" alt="Bigger photo">
+</a>
+<a href="#_" id="lightshot5" class="lightshot">
+    <img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide12.png" alt="Bigger photo">
+</a>
+<a href="#_" id="lightshot6" class="lightshot">
+    <img src="https://inventorstemschool.com/back-up/wp-content/uploads/2021/03/Slide11-1.png" alt="Bigger photo">
+</a>
+
+
+
+
+<script>
+    $( function() {
+        $( "#accordion" ).accordion();
+    } );
+</script>
+<!--[if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif]-->
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+<script>
+    hbspt.forms.create({
+        region: "na1",
+        portalId: "7958870",
+        formId: "1922e630-993f-4d1a-8876-7ee593741b13"
+    });
+</script>
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ page-stem-senior.php
@@ -0,0 +1,161 @@
+<?php /* Template Name: stem-senior 10-16 years */
+get_header(); ?>
+
+<main class="main side-elements">
+    <div class="inner_wrapper"><h1>10-16 years</h1>
+        <section class="courses_type">
+            <div class="courses_about_holder">
+                <div class="courses_about_item courses_about_item_img"><img src="<?php echo get_template_directory_uri(); ?>/img/info5_2.jpg"
+                                                                            alt="Robotics Mindstorms"
+                                                                            class="responsive"></div>
+                <div class="courses_about_item courses_about_item_text"><p class="course_type_head"> Mindstorms
+                        Robotics </p>
+                    <p class="course_age"> Age group: 10+ years </p>
+                    <p class="course_study"> What we study: </p>
+                    <p class="course_info_p"> This is an essential robotics course teaching students to construct more
+                        than 72 LEGO® Mindstorms® EV3 robots, make self-contained robots, and analyze physical and
+                        chemical phenomena through creating models that will help them easily master materials from
+                        future physics and chemistry classes in school. </p>
+                    <p class="course_study"> What do we achieve: </p>
+                    <p class="course_info_p"> As a result of learning, children will gain knowledge about the operation
+                        and structure of a large number of devices and mechanisms, develop the skill to independently
+                        design robots, and program them using visual programming language. They will also develop the
+                        skills of programming algorithms creation, the division of tasks into separate steps, work
+                        planning, and the ability to share duties within the working group. <a href="learn_more" class="den_btn"
+                                                                                               data-toggle="modal"
+                                                                                               data-target="#learn_more">Learn
+                            More</a></p></div>
+            </div>
+            <div class="courses_about_holder">
+                <div class="courses_about_item courses_about_item_text"><p class="course_type_head"> Experiments with
+                        robotics </p>
+                    <p class="course_age"> Age group: 11+ years <br><small>Pre-requisite: Mindstorms Robotics <br>(Terms
+                            1-3)</small></p>
+                    <p class="course_study"> What we study: </p>
+                    <p class="course_info_p"> A logical extension to the “Robotics Mindstorms” course. The course is
+                        extended with new programming constructions featuring new mechanical elements that allow
+                        creating more functional robots. Hence, the program code for robot operation becomes more
+                        complicated. Children learn to improve the written code, quickly identify errors, and understand
+                        the principles of electronic devices operation in relation to written programs. </p>
+                    <p class="course_study"> What do we achieve: </p>
+                    <p class="course_info_p"> Gained knowledge will allow children to make robotic mechanisms on their
+                        own. They will improve their design skills, ability to make mathematical calculations, and use
+                        them for program writing. <a href="learn_more" class="den_btn" data-toggle="modal"
+                                                     data-target="#learn_more">Learn More</a></p></div>
+                <div class="courses_about_item courses_about_item_img"><img src="<?php echo get_template_directory_uri(); ?>/img/info6_2.jpg"
+                                                                            alt="Experiments with Robotics"
+                                                                            class="responsive"></div>
+            </div>
+            <div class="courses_about_holder">
+                <div class="courses_about_item courses_about_item_img"><img src="<?php echo get_template_directory_uri(); ?>/img/sendop1.jpg"
+                                                                            alt="Project Robotics Mindstorms"
+                                                                            class="responsive"></div>
+                <div class="courses_about_item courses_about_item_text"><p class="course_type_head"> Project
+                        Robotics </p>
+                    <p class="course_age"> Age group: 10+ years </p>
+                    <p class="course_study"> What we study: </p>
+                    <p class="course_info_p"> This is an advanced robotics course in which students do not work
+                        according to instructions but control the design of robots on their own. At each lesson, they
+                        will get a task to develop a creative solution using the Lego Mindstorms EV3 kit and program the
+                        designed robot for stand-alone actions. This creative work will help to develop more
+                        sophisticated algorithms and improve programming skills. </p>
+                    <p class="course_study"> What do we achieve: </p>
+                    <p class="course_info_p"> This course develops programming skills, as well as creative thinking and
+                        the ability to solve tasks in different ways. Long-term teamwork teaches children to
+                        communicate, agree, share duties, and take responsibility for a collective result. <a href="learn_more"
+                                                                                                              class="den_btn"
+                                                                                                              data-toggle="modal"
+                                                                                                              data-target="#learn_more">Learn
+                            More</a></p></div>
+                <div class="courses_about_item courses_about_item_text"><p class="course_type_head"> Architecture <br>(Level
+                        1) </p>
+                    <p class="course_age"> Age group: 8-15+ years </p>
+                    <p class="course_study"> What we study: </p>
+                    <p class="course_info_p"> This course is designed for future architects, designers, and engineers.
+                        Children discover world-famous architectural monuments and construct them using customized LEGO®
+                        kits. They study architectural styles and movements and will be able to distinguish them.
+                        Children also learn to work with a pencil, draw a plan, facade, section, and perspective of
+                        buildings and structures. </p>
+                    <p class="course_study"> What do we achieve: </p>
+                    <p class="course_info_p"> Children develop spatial awareness, ability to plan, show their ideas on
+                        paper, and bring them to life. The course involves a lot of teamwork, teaching students to share
+                        duties, listen to each other, and come to an agreement. <a href="learn_more" class="den_btn"
+                                                                                   data-toggle="modal"
+                                                                                   data-target="#learn_more">Learn
+                            More</a></p></div>
+                <div class="courses_about_item courses_about_item_img"><img src="<?php echo get_template_directory_uri(); ?>/img/middop8.jpg"
+                                                                            alt="Architecture level 1"
+                                                                            class="responsive"></div>
+                <div class="courses_about_item courses_about_item_text"><p class="course_type_head"> Architecture <br>(Level
+                        2) </p>
+                    <p class="course_age"> Age group: 9-16+ years </p>
+                    <p class="course_study"> What we study: </p>
+                    <p class="course_info_p"> This course is an extension to “Architecture (Level 1)” course allowing
+                        children to become more familiar with previously learned concepts and learn the new ones.
+                        Students will do some new creative tasks to better understand and use means of composition.
+                        Inventors will also discover the architecture of different countries and times, and plunge into
+                        the world of modern architecture. We will continue to develop skills of drawing sketches and
+                        parti diagrams in sketchbooks. Increased number of creative lessons allows us to fulfill most of
+                        the students’ potential and embody their most daring ideas with LEGO®. </p>
+                    <p class="course_study"> What do we achieve: </p>
+                    <p class="course_info_p"> Children will be able to create their own projects using the given mean of
+                        composition, apply the Fibonacci numbers to create a building, structure, or ensemble. Future
+                        architects will learn the features and periods of styles, deepen knowledge of each of the
+                        composition means, discover volumetric-spatial composition, explore new architectural elements
+                        that are not typical of European countries. Children will improve the ability to work not only
+                        in pairs but also in teams, share duties depending on their skills and abilities. They will
+                        learn to appreciate their projects and works of others. <a href="learn_more" class="den_btn"
+                                                                                   data-toggle="modal"
+                                                                                   data-target="#learn_more">Learn
+                            More</a></p></div>
+                <div class="courses_about_item courses_about_item_img"><img src="<?php echo get_template_directory_uri(); ?>/img/middop9.jpg"
+                                                                            alt="Architecture level 2"
+                                                                            class="responsive"></div>
+                <div class="courses_about_item courses_about_item_text"><p class="course_type_head"> Programming with
+                        <br>Spike Prime </p>
+                    <p class="course_age"> Age group: 9-11 years </p>
+                    <p class="course_study"> What we study: </p>
+                    <p class="course_info_p"> Maneuverability, punctuality, passability, attentiveness - is it possible
+                        to "teach" robots these processes and skills? And does it require special NASA equipment? <br>Our
+                        course "Programming with SPIKE Prime" will convince that programming is not only simple and
+                        interesting, but also possible for every young innovator. <br>The course "Programming with SPIKE
+                        Prime" is developed on the basis of the Spike Prime set - new products of 2020 from LEGO
+                        Education. It opens up opportunities for fun learning programming and robotics using the
+                        easy-to-understand Scratch 3.0 - like programming environment. </p>
+                    <p class="course_study"> What do we achieve: </p>
+                    <p class="course_info_p"> Students will master basic programming skills, learn to program in block
+                        diagrams and understand how to operate variables. <br>Children will create programs for various
+                        robot missions. <br>Understand the technical features of electronic components (color sensor,
+                        force sensor, ultrasound sensor, gyroscope, servomotor) and design and program their own robots.
+                        <br>They will improve communication and presentation skills, learn to distribute and perform
+                        various roles in the team. <a href="learn_more" class="den_btn" data-toggle="modal"
+                                                      data-target="#learn_more">Learn More</a></p></div>
+                <div class="courses_about_item courses_about_item_img"><img src="<?php echo get_template_directory_uri(); ?>/img/spike.jpg" alt="Spike Prime"
+                                                                            class="responsive"></div>
+                <div class="courses_about_item courses_about_item_img"><img src="<?php echo get_template_directory_uri(); ?>/img/middop7.jpg"
+                                                                            alt="Minecraft education"
+                                                                            class="responsive"></div>
+                <div class="courses_about_item courses_about_item_text"><p class="course_type_head"> Programming with
+                        Minecraft: Education Edition </p>
+                    <p class="course_age"> Age group: 8-11 years </p>
+                    <p class="course_study"> What we study: </p>
+                    <p class="course_info_p"> Future programmers will start automating the virtual world of Minecraft
+                        and create their own games in it. To do this children will master the Make Code, a programming
+                        environment with programs consisting of blocks ready to become a real JavaScript code in one
+                        click. Children will learn the basic concepts of programming and develop algorithmic
+                        thinking. </p>
+                    <p class="course_study"> What do we achieve: </p>
+                    <p class="course_info_p"> Students embody their own ideas through programming, understanding the
+                        relations between a created program and events in the world of Minecraft. They gain a skill to
+                        prove the correctness/incorrectness of the algorithms in a logical way, detecting errors in
+                        them. <a href="learn_more" class="den_btn" data-toggle="modal" data-target="#learn_more">Learn More</a>
+                    </p></div>
+            </div>
+        </section>
+    </div>
+</main>
+
+
+<?php get_footer(); ?>
+
+
